e1a69c49d3fceaf736a8d8b62f5ac17184974fd6
hebamosafa <36491097+hebamosafa@users.noreply.github.com>
2020-05-11 03:22:55 +0200

    Add files via upload

--- new/Decorate.py
+++ new/Decorate.py
@@ -29,6 +29,65 @@ from kivy.core.window import Window
 #from android.permissions import request_permissions, Permission
 #request_permissions([Permission.WRITE_EXTERNAL_STORAGE,Permission.READ_EXTERNAL_STORAGE])
 #from android.storage import primary_external_storage_path
+x_clk=0
+y_clk=0
+def Detect_Objects(img):
+    # Yolo algorithm for Detection
+    net = cv2.dnn.readNet("yolov3.weights",
+                          "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    # print(classes)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    height, width, channels = img.shape
+
+    # detect Furniture Image
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # showing info on screen
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    Furniture_list = ["chair", "sofa", "bed", "bench", "diningtable", "refrigerator",
+                      "oven", "microwave", "clock"]
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if ((confidence > 0.5)):
+                # object detected Dimensions
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                width_obj = int(detection[2] * width)
+                height_obj = int(detection[3] * height)
+
+                # rectangle coordinates
+                x_rect = int(center_x - width_obj / 2)
+                y_rect = int(center_y - height_obj / 2)
+
+                boxes.append([x_rect, y_rect, width_obj, height_obj])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    objs = []  # store cropped objects for one image
+
+    for i in range(len(boxes)):
+        if ((i in indexes)):
+            for z in Furniture_list:
+                if classes[class_ids[i]] == z:
+                    x_box, y_box, w_box, h_box = boxes[i]
+                    objs.append(img[y_box: y_box + h_box, x_box: x_box + w_box])
+
+    return (objs)
 SD_CARD = "D:"
 mcolor=(255,255,0,1)
 i=0
@@ -36,17 +95,6 @@ mimage='boy.png'
 captured="obj1.jpg"
 selected="obj2.jpg"
 After="room1.jpg"
-
-    #text = StringProperty('Do you want to save the photo?')
-
-    #ok_text = StringProperty('Save')
-    #cancel_text = StringProperty('Cancel')
-def Mayars(room,obj):
-    print(room)
-    print(obj)
-    cv2.imread("room1.jpg")
-    return "room3.jpg"
-
 class CameraClick(Screen):
     camera=ObjectProperty(None)
     def capture(self):
@@ -80,24 +128,45 @@ def Simple_Wall(image_src='wallimages/wall4_2.jpg', color=(5, 94, 76, 0.2)):
     # Fill in the Wall with the Specific Color
     cv2.drawContours(image, cnts, max_index, color, -1)
     return image
-def Simple_3Walls(image_src='wallimages/wall9.jpg', color=(5, 94, 76, 0.2)):
+
+
+def Simple_3Walls(image_src='wallimages/wall9_3.jpg', color=(5, 94, 76, 0.2)):
     # Read in the image
     image = cv2.imread(image_src)
 
     # change image color to gray scale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    (thresh, blackAndWhiteImage) = cv2.threshold(gray_image, 195, 255, cv2.THRESH_BINARY)
+    image1 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray_image = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(gray_image, 120, 255, cv2.THRESH_BINARY)
 
-    # edges = cv.Canny(gray_image,100,200)
+    lower_white = np.array([229, 229, 229])
+    upper_white = np.array([255, 255, 255])
+    # Define the masked area
+    mask = cv2.inRange(image, lower_white, upper_white)
+
+    newimage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    masked_image = np.copy(newimage)
+    masked_image[mask == 0] = [0, 0, 0]
+
+    # edges = cv2.Canny(gray_image,100,200)
     cnts, hierarchy = cv2.findContours(blackAndWhiteImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
     # Find the index of the largest contour
     areas = [cv2.contourArea(c) for c in cnts]
     max_index = np.argmax(areas)
 
-    # Fill in the Wall with the Specific Color
     cv2.drawContours(image, cnts, max_index, color, -1)
-    return image
+
+    # Load in a background image, and convert it to RGB
+    background_image = image
+    resized = cv2.resize(background_image, (4000, 3000), interpolation=cv2.INTER_AREA)
+
+    new_mask = cv2.bitwise_not(mask)
+    masked_image_bg = cv2.bitwise_and(resized, resized, mask=new_mask)
+
+    complete_image = masked_image_bg + masked_image
+
+    return complete_image
 
 
 def Simple_Chair(image_src='wallimages/wall8.jpg', color=(5, 94, 76, 0.2)):
@@ -139,9 +208,10 @@ def Long_Chair(image_src='wallimages/wall55.jpe', color=(5, 94, 76, 0.2)):
     cv2.drawContours(image, cnts, max_index, color, -1)
     return image
 def send(captured):
-    num_list = ["obj1.jpg", "obj2.jpg", "obj3.jpg"]
-
-    return num_list
+    objects = []
+    img = cv2.imread('sara.jpeg')
+    objects = Detect_Objects(img)
+    return objects
 class ConfirmPopup(Popup):
     text = StringProperty('Do you want to save the photo?')
 
@@ -161,17 +231,13 @@ class ConfirmPopup(Popup):
 
 
     def on_ok(self):
-        y=cv2.imread(mimage)
+        y=cv2.imread(After)
         cv2.imwrite(SD_CARD+"/myph.jpg", y)
         sm.current = "main"
 
 
     def on_cancel(self):
         sm.current = "main"
-class MyPaintWidget(Widget):
-
-    def on_touch_down(self, touch):
-        print("hhhhhhhhhhhhh")
 class P(FloatLayout):
     pass
 def show_popup():
@@ -210,12 +276,14 @@ class CheckImg(Screen):
         img = send(captured)
         self.text="kkkkkkkkkkkkkkkkkkkkkkk"
         for i in img:
-            submit = Button(background_normal=i)
+            cv2.imwrite('tt.jpg',i)
+            submit = Button(background_normal='tt.jpg')
             submit.bind(on_press=self.pressed)
             self.objs.add_widget(submit)
     def pressed(self,i):
         selected=i.background_normal
         print("pressed")
+        self.manager.get_screen("wall").select.opacity = 1
         sm.current="wall"
         print(selected)
 
@@ -223,8 +291,9 @@ class MainWindow(Screen):
 
     def travel(self,new_image):
         #self.ids.mwall.color.opacity = 0
-        global mimage
+        global mimage,After
         mimage = new_image
+        After=mimage
         sm.current="wall"
 
 class WallWindow(Screen):
@@ -236,18 +305,21 @@ class WallWindow(Screen):
     def on_enter (self):
         global mimage
         global After
-        After = mimage
-        self.test.source = mimage
+        #After = mimage
+        self.test.source = After
         self.test.reload()
-        if(self.select.opacity==1):
-            print("hhhhhh")
-            self.mayarsara()
     def mayarsara(self):
-        pass
+        global y_clk,x_clk
+        print("x_clk: "+str(x_clk))
+    def on_touch_down(self,touch):
+        global y_clk,x_clk
+        x_clk=touch.pos[0]
+        y_clk = touch.pos[1]
+        return super(WallWindow, self).on_touch_down(touch)
     def colors(self):
         show_popup()
     def change(self):
-        global mimage
+        global mimage,After
         print("yyyyyyyyyyyy"+mimage)
         global mcolor
         if(mimage=='room1.jpg'):
@@ -259,7 +331,7 @@ class WallWindow(Screen):
         elif(mimage=='room4.jpg'):
             img=Long_Chair(mimage,mcolor)
         print("Entered")
-        After=img
+        After="test2.jpg"
         cv2.imwrite("test2.jpg", img)
         #Image("test2.jpg").save("D:/AVR/test2.jpg")
         self.test.source="test2.jpg"
@@ -297,9 +369,10 @@ class ObjWindow(Screen):
 
     def choose(self,mimg):
         print(mimage)
-        global selected
+        global selected,After
         selected=mimg
         self.manager.get_screen("wall").select.opacity=1
+
         sm.current = "wall"
 class WindowManager(ScreenManager):
     pass
